bcf315015637e3ee28e612aed51085eb61901f77
Brandon Williams <mbrandonw@hey.com>
2022-10-28 16:20:33 -0700

    wip

--- Sources/SwiftUINavigation/NavLink.swift
+++ Sources/SwiftUINavigation/NavLink.swift
@@ -1,65 +1,6 @@
 import CasePaths
 import SwiftUI
 
-@available(iOS 14.0, *)
-public struct LabelWrapper<Enum, Case, Label: View>: View {
-  let label: Label
-  @Binding var `enum`: Enum?
-  let casePath: CasePath<Enum, Case>
-
-  @State var isActive = false
-
-  public var body: some View {
-    self.label
-      .onAppear {
-        self.isActive = self.$enum.isPresent(self.casePath).wrappedValue
-      }
-      .onChange(of: self.isActive) { isActive in
-        guard isActive != self.$enum.isPresent(self.casePath).wrappedValue
-        else { return }
-
-        if isActive {
-          // TODO: anything to do here?
-          //self.onNavigate(isActive)
-        } else {
-          self.enum = nil
-        }
-      }
-      .onChange(of: self.$enum.isPresent(self.casePath).wrappedValue) { isActive in
-        guard isActive != self.isActive
-        else { return }
-
-        self.isActive = isActive
-      }
-  }
-}
-
-@available(iOS 14.0, *)
-extension NavigationLink {
-  public init<Enum, Case, L: View, WD: View>(
-    _unwrapping enum: Binding<Enum?>,
-    case casePath: CasePath<Enum, Case>,
-    onNavigate: @escaping (Bool) -> Void,
-    destination: @escaping (Binding<Case>) -> WD,
-    label: @escaping () -> L
-  ) where Label == LabelWrapper<Enum, Case, L>, Destination == WD? {
-    self.init(
-      unwrapping: `enum`,
-      case: casePath,
-      onNavigate: onNavigate,
-      destination: destination,
-      label: {
-        LabelWrapper(label: label(), enum: `enum`, casePath: casePath)
-      }
-    )
-//    self.enum = `enum`
-//    self.casePath = casePath
-//    self.onNavigate = onNavigate
-//    self.destination = destination
-//    self.label = LabelWrapper(label: label(), enum: `enum`, casePath: casePath)
-  }
-}
-
 @available(iOS, introduced: 14, deprecated: 16)
 @available(macOS, introduced: 10.15, deprecated: 13)
 @available(tvOS, introduced: 13, deprecated: 16)
@@ -71,28 +12,28 @@ public struct NavLink<Enum, Case, Destination: View, Label: View>: View {
   let destination: (Binding<Case>) -> Destination
   let label: Label
 
-//  @State var isActive = false
+  @State var isActive = false
 
-  public init<L>(
+  public init(
     unwrapping enum: Binding<Enum?>,
     case casePath: CasePath<Enum, Case>,
     onNavigate: @escaping (Bool) -> Void,
     destination: @escaping (Binding<Case>) -> Destination,
-    label: @escaping () -> L
-  ) where Label == LabelWrapper<Enum, Case, L> {
+    label: () -> Label
+  ) {
     self.enum = `enum`
     self.casePath = casePath
     self.onNavigate = onNavigate
     self.destination = destination
-    self.label = LabelWrapper(label: label(), enum: `enum`, casePath: casePath)
+    self.label = label()
   }
 
-  public init<L>(
+  public init(
     unwrapping enum: Binding<Enum?>,
     onNavigate: @escaping (Bool) -> Void,
     destination: @escaping (Binding<Case>) -> Destination,
-    label: @escaping () -> L
-  ) where Enum == Case, Label == LabelWrapper<Enum, Case, L> {
+    label: () -> Label
+  ) where Enum == Case {
     self.init(
       unwrapping: `enum`,
       case: /.self,
@@ -103,24 +44,24 @@ public struct NavLink<Enum, Case, Destination: View, Label: View>: View {
   }
 
   public var body: some View {
-    NavigationLink(isActive: self.enum.isPresent(self.casePath)) {
+    NavigationLink(isActive: self.$isActive) {
       Binding(unwrapping: self.enum.case(self.casePath)).map(self.destination)
     } label: {
       self.label
     }
-//    .onAppear {
-//      self.isActive = self.enum.isPresent(self.casePath).wrappedValue
-//    }
-//    .onChange(of: self.isActive) { isActive in
-//      if isActive {
-//        // TODO: anything to do here?
-//        //self.onNavigate(isActive)
-//      } else {
-//        self.enum.wrappedValue = nil
-//      }
-//    }
-//    .onChange(of: self.enum.isPresent(self.casePath).wrappedValue) { isActive in
-//      self.isActive = isActive
-//    }
+    .onAppear {
+      self.isActive = self.enum.isPresent(self.casePath).wrappedValue
+    }
+    .onChange(of: self.isActive) { isActive in
+      if isActive {
+        // TODO: anything to do here?
+        //self.onNavigate(isActive)
+      } else {
+        self.enum.wrappedValue = nil
+      }
+    }
+    .onChange(of: self.enum.isPresent(self.casePath).wrappedValue) { isActive in
+      self.isActive = isActive
+    }
   }
 }
