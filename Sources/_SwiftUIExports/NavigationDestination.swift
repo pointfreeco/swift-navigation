dc193cc8f40682cab12ace7f638a11a07583948e
Brandon Williams <mbrandonw@hey.com>
2022-10-31 14:56:58 -0700

    wip

--- Sources/_SwiftUIExports/NavigationDestination.swift
+++ Sources/_SwiftUIExports/NavigationDestination.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @available(iOS 16, macOS 13, tvOS 16, watchOS 9, *)
 extension View {
+  // TODO: hide behind @_spi?
   public func _navigationDestination<V: View>(
     isPresented: Binding<Bool>,
     @ViewBuilder destination: () -> V
