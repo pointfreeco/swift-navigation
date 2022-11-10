bbb8f514ae1c47ebe2e08192a0eaac03973dae75
Brandon Williams <mbrandonw@hey.com>
2022-11-10 09:29:23 -0500

    wip

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
     )
   ],
   dependencies: [
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.10.0"),
     .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.6.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.5.0"),
