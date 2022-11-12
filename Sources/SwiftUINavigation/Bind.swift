00a10b4e5318706d85a545c775784e64c3dbbdea
Stephen Celis <stephen@stephencelis.com>
2022-11-11 17:13:19 -0800

    wip

--- Sources/SwiftUINavigation/Bind.swift
+++ Sources/SwiftUINavigation/Bind.swift
@@ -1,6 +1,20 @@
 import SwiftUI
 
 extension View {
+  /// Synchronizes model state to view state via two-way bindings.
+  ///
+  /// SwiftUI comes with many property wrappers that can be used in views to drive view state, like
+  /// field focus. Unfortunately, these property wrappers _must_ be used in views. It's not possible
+  /// to extract this logic to an observable object and integrate it with the rest of the model's
+  /// business logic, and be in a better position to test this state.
+  ///
+  /// We can work around these limitations by introducing a published field to your observable
+  /// object and synchronizing it to view state with this view modifier.
+  ///
+  /// - Parameters:
+  ///   - modelValue: A binding from model state. _E.g._, a binding derived from a published field
+  ///     on an observable object.
+  ///   - viewValue: A binding from view state. _E.g._, a focus binding.
   @available(iOS 14, macOS 11, tvOS 14, watchOS 7, *)
   public func bind<ModelValue: _Bindable, ViewValue: _Bindable>(
     _ modelValue: ModelValue, to viewValue: ViewValue
