2eb70bab6c9c1e3f74f4a0c189b08a8205d28b49
Stephen Celis <stephen@stephencelis.com>
2023-01-09 11:22:00 -0500

    wip

--- Examples/Standups/Standups/Helpers.swift
+++ Examples/Standups/Standups/Helpers.swift
@@ -34,8 +34,11 @@ struct Preview<Content: View>: View {
 struct Preview_Previews: PreviewProvider {
   static var previews: some View {
     Preview(
-      message:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      message: """
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        """
     ) {
       StandupDetailView(model: StandupDetailModel(standup: .mock))
     }
