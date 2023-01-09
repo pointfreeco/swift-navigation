7fa6d2d8311ba18225c4c0ab17bd43362a114230
Brandon Williams <mbrandonw@hey.com>
2023-01-09 08:22:26 -0800

    wip

--- Examples/Standups/Standups/Helpers.swift
+++ Examples/Standups/Standups/Helpers.swift
@@ -34,7 +34,8 @@ struct Preview<Content: View>: View {
 struct Preview_Previews: PreviewProvider {
   static var previews: some View {
     Preview(
-      message: """
+      message: 
+        """
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
         ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
         ullamco laboris nisi ut aliquip ex ea commodo consequat.
