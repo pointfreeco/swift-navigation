270a754308f5440be52fc295242eb7031638bd15
Vincent Isambart <vincent.isambart@gmail.com>
2023-01-31 05:39:42 +0900

    Real dependencies (#74)
    * Move XCTestDynamicOverlay to real place of use
    
    * Remove unused dependencies

--- Package.swift
+++ Package.swift
@@ -30,8 +30,8 @@ let package = Package(
     .target(
       name: "_SwiftUINavigationState",
       dependencies: [
-        .product(name: "CasePaths", package: "swift-case-paths"),
         .product(name: "CustomDump", package: "swift-custom-dump"),
+        .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay"),
       ]
     ),
     .target(
@@ -39,8 +39,6 @@ let package = Package(
       dependencies: [
         "_SwiftUINavigationState",
         .product(name: "CasePaths", package: "swift-case-paths"),
-        .product(name: "CustomDump", package: "swift-custom-dump"),
-        .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay"),
       ]
     ),
     .testTarget(
