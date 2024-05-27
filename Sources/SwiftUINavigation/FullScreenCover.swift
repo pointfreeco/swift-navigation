72dbb2a5cac2e877242c553a3b4ad6840fe33da9
Stephen Celis <stephen@stephencelis.com>
2024-05-27 14:18:18 -0700

    Add `sheet(item:id:)` (#155)
    * Add `sheet(item:id:)`
    
    `ForEach` has a convenient initializer that takes a key path to some
    hashable identifier so that the element isn't forced into an
    identifiable conformance, but `sheet`, `fullScreenCover` and `popover`
    aren't given the same affordances. Let's close the gap.
    
    * fix

--- Sources/SwiftUINavigation/FullScreenCover.swift
+++ Sources/SwiftUINavigation/FullScreenCover.swift
@@ -1,7 +1,31 @@
 #if canImport(SwiftUI)
   import SwiftUI
 
+  @available(iOS 14, tvOS 14, watchOS 7, *)
+  @available(macOS, unavailable)
   extension View {
+    /// Presents a full-screen cover using a binding as a data source for the sheet's content based
+    /// on the identity of the underlying item.
+    ///
+    /// - Parameters:
+    ///   - item: A binding to an optional source of truth for the sheet. When `item` is non-`nil`,
+    ///     the system passes the item's content to the modifier's closure. You display this content
+    ///     in a sheet that you create that the system displays to the user. If `item` changes, the
+    ///     system dismisses the sheet and replaces it with a new one using the same process.
+    ///   - id: The key path to the provided item's identifier.
+    ///   - onDismiss: The closure to execute when dismissing the sheet.
+    ///   - content: A closure returning the content of the sheet.
+    public func fullScreenCover<Item, ID: Hashable, Content: View>(
+      item: Binding<Item?>,
+      id: KeyPath<Item, ID>,
+      onDismiss: (() -> Void)? = nil,
+      @ViewBuilder content: @escaping (Item) -> Content
+    ) -> some View {
+      self.fullScreenCover(item: item[id: id], onDismiss: onDismiss) { _ in
+        item.wrappedValue.map(content)
+      }
+    }
+
     /// Presents a full-screen cover using a binding as a data source for the sheet's content.
     ///
     /// SwiftUI comes with a `fullScreenCover(item:)` view modifier that is powered by a binding to
@@ -36,15 +60,13 @@
     ///
     /// - Parameters:
     ///   - value: A binding to a source of truth for the sheet. When `value` is non-`nil`, a
-    ///     non-optional binding to the value is passed to the `content` closure. You use this binding
-    ///     to produce content that the system presents to the user in a sheet. Changes made to the
-    ///     sheet's binding will be reflected back in the source of truth. Likewise, changes to
-    ///     `value` are instantly reflected in the sheet. If `value` becomes `nil`, the sheet is
+    ///     non-optional binding to the value is passed to the `content` closure. You use this
+    ///     binding to produce content that the system presents to the user in a sheet. Changes made
+    ///     to the sheet's binding will be reflected back in the source of truth. Likewise, changes
+    ///     to `value` are instantly reflected in the sheet. If `value` becomes `nil`, the sheet is
     ///     dismissed.
     ///   - onDismiss: The closure to execute when dismissing the sheet.
     ///   - content: A closure returning the content of the sheet.
-    @available(iOS 14, tvOS 14, watchOS 7, *)
-    @available(macOS, unavailable)
     public func fullScreenCover<Value, Content>(
       unwrapping value: Binding<Value?>,
       onDismiss: (() -> Void)? = nil,
