43af80b0be4e833e7ccf590b83409920a15d7120
Brandon Williams <mbrandonw@hey.com>
2024-05-31 14:34:41 -0500

    added some tests

---
+++ Examples/UIKitCaseStudiesTests/NavigationPathTests.swift
@@ -0,0 +1,8 @@
+import UIKitNavigation
+import XCTest
+
+final class NavigationPathTests: XCTestCase {
+  @MainActor
+  func testBasics() async throws {
+  }
+}
--- Examples/UIKitCaseStudiesTests/NavigationStackTests.swift
+++ Examples/UIKitCaseStudiesTests/NavigationStackTests.swift
@@ -3,13 +3,13 @@ import XCTest
 
 final class NavigationStackTests: XCTestCase {
   @MainActor
-  func testBasics() async throws {
+  func testMutatingBinding() async throws {
     @UIBindable var model = Model()
     let nav = NavigationStackController(path: $model.path) {
       UIViewController()
     }
-    nav.navigationDestination(for: Int.self) { _ in
-      UIViewController()
+    nav.navigationDestination(for: Int.self) { number in
+      ChildViewController(number: number)
     }
     try setUp(controller: nav)
 
@@ -37,8 +37,8 @@ final class NavigationStackTests: XCTestCase {
     let nav = NavigationStackController(path: $model.path) {
       UIViewController()
     }
-    nav.navigationDestination(for: Int.self) { _ in
-      UIViewController()
+    nav.navigationDestination(for: Int.self) { number in
+      ChildViewController(number: number)
     }
     try setUp(controller: nav)
 
@@ -55,6 +55,74 @@ final class NavigationStackTests: XCTestCase {
       nav.viewControllers.count == 3
     }
   }
+
+  @MainActor
+  func testDeepLink() async throws {
+    @UIBindable var model = Model(path: [1, 2, 3])
+    let nav = NavigationStackController(path: $model.path) {
+      UIViewController()
+    }
+    nav.navigationDestination(for: Int.self) { number in
+      ChildViewController(number: number)
+    }
+    try setUp(controller: nav)
+
+    await assertEventually {
+      nav.viewControllers.count == 4
+    }
+    XCTAssertEqual(
+      nav.viewControllers.compactMap { ($0 as? ChildViewController)?.number },
+      [1, 2, 3]
+    )
+  }
+
+  @MainActor
+  func testManualPopLast() async throws {
+    @UIBindable var model = Model()
+    let nav = NavigationStackController(path: $model.path) {
+      UIViewController()
+    }
+    nav.navigationDestination(for: Int.self) { number in
+      ChildViewController(number: number)
+    }
+    try setUp(controller: nav)
+
+    model.path.append(1)
+    await assertEventually {
+      nav.viewControllers.count == 2
+    }
+    try await Task.sleep(for: .seconds(0.1))
+    nav.popViewController(animated: false)
+    await assertEventually {
+      model.path == []
+    }
+    await assertEventually {
+      nav.viewControllers.count == 1
+    }
+  }
+
+  @MainActor
+  func testManualPopMiddle() async throws {
+    @UIBindable var model = Model(path: [1, 2, 3, 4])
+    let nav = NavigationStackController(path: $model.path) {
+      UIViewController()
+    }
+    nav.navigationDestination(for: Int.self) { number in
+      ChildViewController(number: number)
+    }
+    try setUp(controller: nav)
+
+    await assertEventually {
+      nav.viewControllers.count == 5
+    }
+    nav.popToViewController(nav.viewControllers[2], animated: false)
+    await assertEventually {
+      model.path == [1, 2]
+    }
+    await assertEventually {
+      nav.viewControllers.count == 3
+    }
+  }
 }
 
 @Observable
@@ -62,5 +130,16 @@ private class Model {
   var path: [Int]
   init(path: [Int] = []) {
     self.path = path
+  }
+}
+
+private final class ChildViewController: UIViewController {
+  let number: Int
+  init(number: Int) {
+    self.number = number
+    super.init(nibName: nil, bundle: nil)
+  }
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
   }
 }
