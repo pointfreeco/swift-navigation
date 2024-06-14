0f7483551bf53d105bc034841f87f76cb92de560
Stephen Celis <stephen@stephencelis.com>
2024-06-14 12:39:15 -0700

    Swift 6 Language Mode

---
+++ Package@swift-6.0.swift
@@ -0,0 +1,53 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+  name: "swiftui-navigation",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+    .tvOS(.v13),
+    .watchOS(.v6),
+  ],
+  products: [
+    .library(
+      name: "SwiftUINavigation",
+      targets: ["SwiftUINavigation"]
+    ),
+    .library(
+      name: "SwiftUINavigationCore",
+      targets: ["SwiftUINavigationCore"]
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+//    .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.2.2"),
+    .package(path: "../swift-case-paths"),
+    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0"),
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0"),
+  ],
+  targets: [
+    .target(
+      name: "SwiftUINavigation",
+      dependencies: [
+        "SwiftUINavigationCore",
+        .product(name: "CasePaths", package: "swift-case-paths"),
+      ]
+    ),
+    .testTarget(
+      name: "SwiftUINavigationTests",
+      dependencies: [
+        "SwiftUINavigation"
+      ]
+    ),
+    .target(
+      name: "SwiftUINavigationCore",
+      dependencies: [
+        .product(name: "CustomDump", package: "swift-custom-dump"),
+        .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay"),
+      ]
+    ),
+  ],
+  swiftLanguageVersions: [.v6]
+)
