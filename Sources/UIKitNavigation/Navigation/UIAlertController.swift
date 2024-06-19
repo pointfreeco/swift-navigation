c3a10172f8559242c8a906674e40a07cd212dc65
Cosmic Flamingo <acosmicflamingo@gmail.com>
2024-06-19 16:32:33 -0500

    Make UIAlertAction convenience initializers public

--- Sources/UIKitNavigation/Navigation/UIAlertController.swift
+++ Sources/UIKitNavigation/Navigation/UIAlertController.swift
@@ -53,7 +53,7 @@
   @available(tvOS 13, *)
   @available(watchOS, unavailable)
   extension UIAlertAction.Style {
-    init(_ role: ButtonStateRole) {
+    public init(_ role: ButtonStateRole) {
       switch role {
       case .cancel:
         self = .cancel
@@ -69,7 +69,7 @@
   @available(tvOS 13, *)
   @available(watchOS, unavailable)
   extension UIAlertAction {
-    convenience init<Action>(
+    public convenience init<Action>(
       _ button: ButtonState<Action>,
       action handler: @escaping (_ action: Action?) -> Void
     ) {
