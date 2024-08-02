6268fcfe0ae67aa8fe2a9e609296321fd70626bf
Stephen Celis <stephen@stephencelis.com>
2024-08-02 09:38:59 -0700

    wip

--- Tests/UIKitNavigationTests/ObserveTests.swift
+++ Tests/UIKitNavigationTests/ObserveTests.swift
@@ -1,13 +1,15 @@
-import UIKitNavigation
-import XCTest
+#if canImport(UIKit)
+  import UIKitNavigation
+  import XCTest
 
-class ObserveTests: XCTestCase {
-  @MainActor
-  func testCompiles() {
-    var count = 0
-    observe {
-      count = 1
+  class ObserveTests: XCTestCase {
+    @MainActor
+    func testCompiles() {
+      var count = 0
+      observe {
+        count = 1
+      }
+      XCTAssertEqual(count, 1)
     }
-    XCTAssertEqual(count, 1)
   }
-}
+#endif
