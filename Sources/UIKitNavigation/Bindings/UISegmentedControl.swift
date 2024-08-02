537f14021aed773c9d98140336d60adc766378bb
Stephen Celis <stephen@stephencelis.com>
2024-08-02 09:19:45 -0700

    wip

--- Sources/UIKitNavigation/Bindings/UISegmentedControl.swift
+++ Sources/UIKitNavigation/Bindings/UISegmentedControl.swift
@@ -1,4 +1,5 @@
 #if canImport(UIKit) && !os(watchOS)
+  import IssueReporting
   import UIKit
 
   @available(iOS 14, tvOS 14, *)
@@ -11,10 +12,20 @@
     ///   - selectedSegment: The binding to read from for the selected color, and write to when the
     ///     selected color is changes.
     public convenience init(
-      frame: CGRect = .zero, selectedSegment: UIBinding<some RawRepresentable<Int>>
+      frame: CGRect = .zero, selectedSegment: UIBinding<some RawRepresentable<Int>>,
+      fileID: StaticString = #fileID,
+      filePath: StaticString = #filePath,
+      line: UInt = #line,
+      column: UInt = #column
     ) {
       self.init(frame: frame)
-      bind(selectedSegment: selectedSegment)
+      bind(
+        selectedSegment: selectedSegment,
+        fileID: fileID,
+        filePath: filePath,
+        line: line,
+        column: column
+      )
     }
 
     /// Establishes a two-way connection between a binding and the color well's selected color.
@@ -23,18 +34,44 @@
     ///   when the selected color changes.
     /// - Returns: A cancel token.
     @discardableResult
-    public func bind(selectedSegment: UIBinding<some RawRepresentable<Int>>) -> ObservationToken {
-      bind(selectedSegment.toRawValue, to: \.selectedSegmentIndex, for: .valueChanged)
+    public func bind(
+      selectedSegment: UIBinding<some RawRepresentable<Int>>,
+      fileID: StaticString = #fileID,
+      filePath: StaticString = #filePath,
+      line: UInt = #line,
+      column: UInt = #column
+    ) -> ObservationToken {
+      let fileID = HashableStaticString(rawValue: fileID)
+      let filePath = HashableStaticString(rawValue: filePath)
+      return bind(
+        selectedSegment[fileID: fileID, filePath: filePath, line: line, column: column],
+        to: \.selectedSegmentIndex,
+        for: .valueChanged
+      )
     }
   }
 
   extension RawRepresentable<Int> {
-    fileprivate var toRawValue: Int {
+    fileprivate subscript(
+      fileID fileID: HashableStaticString,
+      filePath filePath: HashableStaticString,
+      line line: UInt,
+      column column: UInt
+    ) -> Int {
       get { rawValue }
       set {
         guard let rawRepresentable = Self(rawValue: newValue)
         else {
-          // TODO: `runtimeWarn`
+          reportIssue(
+            """
+            Raw-representable 'UIBinding<\(Self.self)>' attempted to write an invalid raw value \
+            ('\(newValue)').
+            """,
+            fileID: fileID.rawValue,
+            filePath: filePath.rawValue,
+            line: line,
+            column: column
+          )
           return
         }
         self = rawRepresentable
