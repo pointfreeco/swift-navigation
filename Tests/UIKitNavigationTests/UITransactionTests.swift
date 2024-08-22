c9081d36fd31570e04ae93145f080b701695b2a5
Brandon Williams <mbrandonw@hey.com>
2024-08-22 18:32:57 -0400

    fix test

--- Tests/UIKitNavigationTests/UITransactionTests.swift
+++ Tests/UIKitNavigationTests/UITransactionTests.swift
@@ -4,35 +4,36 @@
   import XCTest
 
   class UITransactionTests: XCTestCase {
+    #if !os(watchOS)
+      @MainActor
+      func testTransactionKeyPropagatesWithAnimation() async {
+        let expectation = expectation(description: "onChange")
+        expectation.expectedFulfillmentCount = 2
 
-    @MainActor
-    func testTransactionKeyPropagatesWithAnimation() async {
-      let expectation = expectation(description: "onChange")
-      expectation.expectedFulfillmentCount = 2
+        let model = Model()
+        XCTAssertEqual(UITransaction.current.isSet, false)
 
-      let model = Model()
-      XCTAssertEqual(UITransaction.current.isSet, false)
-
-      observe {
-        if model.count == 0 {
-          XCTAssertEqual(UITransaction.current.isSet, false)
-        } else if model.count == 1 {
-          XCTAssertEqual(UITransaction.current.isSet, true)
-        } else {
-          XCTFail()
+        observe {
+          if model.count == 0 {
+            XCTAssertEqual(UITransaction.current.isSet, false)
+          } else if model.count == 1 {
+            XCTAssertEqual(UITransaction.current.isSet, true)
+          } else {
+            XCTFail()
+          }
+          expectation.fulfill()
         }
-        expectation.fulfill()
-      }
 
-      withUITransaction(\.isSet, true) {
-        withUIKitAnimation {
-          model.count += 1
+        withUITransaction(\.isSet, true) {
+          withUIKitAnimation {
+            model.count += 1
+          }
         }
+        await fulfillment(of: [expectation], timeout: 1)
+        XCTAssertEqual(model.count, 1)
+        XCTAssertEqual(UITransaction.current.isSet, false)
       }
-      await fulfillment(of: [expectation], timeout: 1)
-      XCTAssertEqual(model.count, 1)
-      XCTAssertEqual(UITransaction.current.isSet, false)
-    }
+    #endif
   }
 
   @Perceptible
