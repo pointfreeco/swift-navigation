6261bfd283995da540939d2842f9f2b055484258
Stephen Celis <stephen@stephencelis.com>
2024-08-22 14:41:20 -0700

    wip

--- Examples/CaseStudies/CaseStudiesApp.swift
+++ Examples/CaseStudies/CaseStudiesApp.swift
@@ -5,34 +5,6 @@ struct CaseStudiesApp: App {
   var body: some Scene {
     WindowGroup {
       RootView()
-//      ContentView()
     }
   }
-}
-
-import UIKitNavigation
-struct ContentView: View {
-  @State var isHidden = false
-  var body: some View {
-    Form {
-      Text("Hello!")
-        .opacity(isHidden ? 0 : 1 )
-
-      // animations: [PlatformKey: ]
-//      perform.uiKit
-//      perform.wasm
-//      perform.uiKit
-
-      Button("Tap") {
-        withUIKitAnimation(.linear) {
-          withAnimation(.linear(duration: 0.3)) {
-            isHidden.toggle()
-          }
-        }
-      }
-    }
-  }
-}
-#Preview {
-
 }
