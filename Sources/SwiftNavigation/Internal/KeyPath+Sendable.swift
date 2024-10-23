07a99a915ed7e0a9ffc5d6165bf2b5e3087ac861
Stephen Celis <stephen@stephencelis.com>
2024-10-23 11:16:28 -0700

    Avoid sendable key paths in dynamic member lookup (#245)
    There are a few compiler bugs that prevent us from declaring sendability
    for key paths where it's needed.
    
    First, doing so breaks autocomplete, which really hurts the developer
    experience: https://github.com/swiftlang/swift/issues/77035
    
    Second, even though recovering autocomplete might be preferable at the
    cost of safety, there is no safety to begin with right now because
    sendable diagnostics don't propagate through dynamic member lookup:
    https://github.com/swiftlang/swift/issues/77105
    
    Because of this, let's only use non-sendable key paths for now, and
    force cast them under the hood.

--- Sources/SwiftNavigation/Internal/KeyPath+Sendable.swift
+++ Sources/SwiftNavigation/Internal/KeyPath+Sendable.swift
@@ -6,3 +6,30 @@
   public typealias _SendableKeyPath<Root, Value> = KeyPath<Root, Value>
   public typealias _SendableWritableKeyPath<Root, Value> = WritableKeyPath<Root, Value>
 #endif
+
+// NB: Dynamic member lookup does not currently support sendable key paths and even breaks
+//     autocomplete.
+//
+//     * https://github.com/swiftlang/swift/issues/77035
+//     * https://github.com/swiftlang/swift/issues/77105
+extension _AppendKeyPath {
+  @_transparent
+  func unsafeSendable<Root, Value>() -> _SendableKeyPath<Root, Value>
+  where Self == KeyPath<Root, Value> {
+    #if compiler(>=6)
+      unsafeBitCast(self, to: _SendableKeyPath<Root, Value>.self)
+    #else
+      self
+    #endif
+  }
+
+  @_transparent
+  func unsafeSendable<Root, Value>() -> _SendableWritableKeyPath<Root, Value>
+  where Self == WritableKeyPath<Root, Value> {
+    #if compiler(>=6)
+      unsafeBitCast(self, to: _SendableWritableKeyPath<Root, Value>.self)
+    #else
+      self
+    #endif
+  }
+}
