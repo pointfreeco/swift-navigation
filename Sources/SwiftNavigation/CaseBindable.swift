42a7af8d9ac6d3f6a512e81ee89e781b0ee1b607
Stephen Celis <stephen@stephencelis.com>
2026-06-22 12:18:55 -0700

    Add `@UITransactionEntry` macro (#351)
    * Add `@UITransactionEntry` macro
    
    * fix
    
    * fix

--- Sources/SwiftNavigation/CaseBindable.swift
+++ Sources/SwiftNavigation/CaseBindable.swift
@@ -1,18 +1,30 @@
 #if CasePaths
   public import CasePaths
 
-  public protocol CaseBindable: CasePathable {
-    associatedtype UIBindingEnumeration
-
-    static func _$caseBinding(_ binding: UIBinding<Self>) -> UIBindingEnumeration
-
-    #if canImport(SwiftUI)
-      associatedtype BindingEnumeration
-
-      static func _$caseBinding(_ binding: SwiftUI.Binding<Self>) -> BindingEnumeration
-    #endif
-  }
-
+  /// Defines and implements conformance of the `CaseBindable` protocol.
+  ///
+  /// This macro adds binding support to an enumeration by conforming it to the ``CaseBindable``
+  /// protocol. For example, the following code applies the `@CaseBindable` macro to the type
+  /// `Item.Status`, making it possible to switch over bindings to each case of the enum:
+  ///
+  /// ```swift
+  /// struct Item {
+  ///   @CaseBindable enum Status {
+  ///     case inStock(quantity: Int)
+  ///     case outOfStock(isOnBackOrder: Bool)
+  ///   }
+  ///   var status: Status
+  /// }
+  /// // ...
+  /// @Binding var item: Item
+  /// // ...
+  /// switch $item.status {
+  /// case .inStock(let $quantity):
+  ///   Stepper("Quantity: \($quantity.wrappedValue)", value: $quantity)
+  /// case .outOfStock(let $isOnBackOrder):
+  ///   Toggle("Is on back order?", isOn: $isOnBackOrder)
+  /// }
+  /// ```
   @attached(extension, conformances: CasePathable, CasePathIterable, CaseBindable)
   @attached(
     member,
@@ -26,13 +38,56 @@
   public macro CaseBindable() =
     #externalMacro(module: "SwiftNavigationMacros", type: "CaseBindableMacro")
 
+  /// A type whose binding can be switched over exhaustively.
+  ///
+  /// Use the ``CaseBindable()`` macro to generate a conformance.
+  public protocol CaseBindable: CasePathable {
+    associatedtype UIBindingEnumeration
+
+    static func _$caseBinding(_ binding: UIBinding<Self>) -> UIBindingEnumeration
+
+    #if canImport(SwiftUI)
+      associatedtype BindingEnumeration
+
+      static func _$caseBinding(_ binding: SwiftUI.Binding<Self>) -> BindingEnumeration
+    #endif
+  }
+
   extension UIBinding where Value: CaseBindable {
+    /// An enumeration of bindings that can be switched over exhaustively.
+    ///
+    /// ```swift
+    /// @UIBinding var status: Status
+    /// // ...
+    /// switch $status.cases {
+    /// case .inStock(let $quantity):
+    ///   Stepper("Quantity: \($quantity.wrappedValue)", value: $quantity)
+    /// case .outOfStock(let $isOnBackOrder):
+    ///   Toggle("Is on back order?", isOn: $isOnBackOrder)
+    /// }
+    /// ```
     public var cases: Value.UIBindingEnumeration {
       Value._$caseBinding(self)
     }
   }
 
   extension UIBinding {
+    /// Derives an enumeration of bindings that can be switched over exhaustively _via_ dynamic
+    /// member lookup.
+    ///
+    /// You don't call this subscript directly. Instead, Swift calls it for you when you access a
+    /// property from the underlying `Value` directly on this binding:
+    ///
+    /// ```swift
+    /// @UIBinding var item: Item
+    /// // ...
+    /// switch $item.status {
+    /// case .inStock(let $quantity):
+    ///   Stepper("Quantity: \($quantity.wrappedValue)", value: $quantity)
+    /// case .outOfStock(let $isOnBackOrder):
+    ///   Toggle("Is on back order?", isOn: $isOnBackOrder)
+    /// }
+    /// ```
     public subscript<Member: CaseBindable>(
       dynamicMember keyPath: WritableKeyPath<Value, Member>
     ) -> Member.UIBindingEnumeration {
@@ -42,6 +97,22 @@
   }
 
   extension UIBindable {
+    /// Derives an enumeration of bindings that can be switched over exhaustively _via_ dynamic
+    /// member lookup.
+    ///
+    /// You don't call this subscript directly. Instead, Swift calls it for you when you access a
+    /// property from the underlying `Value` directly on this bindable object:
+    ///
+    /// ```swift
+    /// @UIBindable var item: Item
+    /// // ...
+    /// switch $item.status {
+    /// case .inStock(let $quantity):
+    ///   Stepper("Quantity: \($quantity.wrappedValue)", value: $quantity)
+    /// case .outOfStock(let $isOnBackOrder):
+    ///   Toggle("Is on back order?", isOn: $isOnBackOrder)
+    /// }
+    /// ```
     public subscript<Member: CaseBindable>(
       dynamicMember keyPath: ReferenceWritableKeyPath<Value, Member>
     ) -> Member.UIBindingEnumeration where Value: AnyObject {
@@ -64,12 +135,40 @@
     public typealias _Binding<Value> = SwiftUI.Binding<Value>
 
     extension SwiftUI.Binding where Value: CaseBindable {
+      /// An enumeration of bindings that can be switched over exhaustively.
+      ///
+      /// ```swift
+      /// @Binding var status: Status
+      /// // ...
+      /// switch $status.cases {
+      /// case .inStock(let $quantity):
+      ///   Stepper("Quantity: \($quantity.wrappedValue)", value: $quantity)
+      /// case .outOfStock(let $isOnBackOrder):
+      ///   Toggle("Is on back order?", isOn: $isOnBackOrder)
+      /// }
+      /// ```
       public var cases: Value.BindingEnumeration {
         Value._$caseBinding(self)
       }
     }
 
     extension SwiftUI.Binding {
+      /// Derives an enumeration of bindings that can be switched over exhaustively _via_ dynamic
+      /// member lookup.
+      ///
+      /// You don't call this subscript directly. Instead, Swift calls it for you when you access a
+      /// property from the underlying `Value` directly on this binding:
+      ///
+      /// ```swift
+      /// @Binding var item: Item
+      /// // ...
+      /// switch $item.status {
+      /// case .inStock(let $quantity):
+      ///   Stepper("Quantity: \($quantity.wrappedValue)", value: $quantity)
+      /// case .outOfStock(let $isOnBackOrder):
+      ///   Toggle("Is on back order?", isOn: $isOnBackOrder)
+      /// }
+      /// ```
       public subscript<Member: CaseBindable>(
         dynamicMember keyPath: WritableKeyPath<Value, Member>
       ) -> Member.BindingEnumeration {
@@ -80,6 +179,22 @@
 
     @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
     extension SwiftUI.Bindable {
+      /// Derives an enumeration of bindings that can be switched over exhaustively _via_ dynamic
+      /// member lookup.
+      ///
+      /// You don't call this subscript directly. Instead, Swift calls it for you when you access a
+      /// property from the underlying `Value` directly on this bindable object:
+      ///
+      /// ```swift
+      /// @Bindable var item: Item
+      /// // ...
+      /// switch $item.status {
+      /// case .inStock(let $quantity):
+      ///   Stepper("Quantity: \($quantity.wrappedValue)", value: $quantity)
+      /// case .outOfStock(let $isOnBackOrder):
+      ///   Toggle("Is on back order?", isOn: $isOnBackOrder)
+      /// }
+      /// ```
       public subscript<Member: CaseBindable>(
         dynamicMember keyPath: ReferenceWritableKeyPath<Value, Member>
       ) -> Member.BindingEnumeration where Value: AnyObject {
