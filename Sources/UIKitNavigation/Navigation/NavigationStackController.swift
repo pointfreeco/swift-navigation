6454ca5b64038ea3f8f9ba3ccfff468c53c88eca
Stephen Celis <stephen@stephencelis.com>
2026-07-01 17:21:15 -0700

    fix

--- Sources/UIKitNavigation/Navigation/NavigationStackController.swift
+++ Sources/UIKitNavigation/Navigation/NavigationStackController.swift
@@ -375,6 +375,9 @@
       stackController.path.append(.lazy(.element(value)))
     }
 
+    #if !Perception
+      @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
+    #endif
     public func navigationDestination<D: Hashable>(
       for data: D.Type,
       destination: @escaping (D) -> UIViewController
